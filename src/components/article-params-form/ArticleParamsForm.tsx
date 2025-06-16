3d09a658e07f63496297b92d50d483856f13f2e7
Anna Mamedov <mamedovaanna.i@gmail.com>
2025-06-16 12:22:56 +0300

    попытка сдача пр

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,103 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useState, useRef } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+import { useOutsideClickClose } from '../../ui/select/hooks/useOutsideClickClose';
+import { OptionType, ArticleStateType,
+	 fontColors, fontFamilyOptions, backgroundColors,
+	  contentWidthArr, fontSizeOptions, defaultArticleState } from 'src/constants/articleProps';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import { RadioGroup } from 'src/ui/radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currentArticleState: ArticleStateType;
+	setCurrentArticleState: (param: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = ({
+	currentArticleState,
+	setCurrentArticleState,
+	}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLDivElement>(null);
+	const [selectArticleState, setSelectArticleState] = useState<ArticleStateType>(currentArticleState);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setSelectArticleState((prev) => ({
+		...prev,
+		[key]: value,
+		}));
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
+		<div ref={rootRef} >
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(prev => !prev)} />
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
+			<form
+				className={styles.form}
+				onSubmit={(e) => {
+				e.preventDefault();
+				setCurrentArticleState(selectArticleState);
+				setIsOpen(false);
+				}}
+				onReset={() => {
+					setCurrentArticleState(defaultArticleState),
+					setSelectArticleState(defaultArticleState)
+				}}
+			>
+				<Text size={31}>ЗАДАЙТЕ ПАРАМЕТРЫ</Text>
+
+				<Select
+				selected={selectArticleState.fontFamilyOption}
+				options={fontFamilyOptions}
+				onChange={(option) => handleChange('fontFamilyOption', option)}
+				title="Шрифт"
+				/>
+
+				<RadioGroup
+				name='radioButtons'
+				selected={selectArticleState.fontSizeOption}
+				options={fontSizeOptions}
+				onChange={(option) => handleChange('fontSizeOption', option)}
+				title="Размер шрифта"
+				/>
+
+				<Select
+				selected={selectArticleState.fontColor}
+				options={fontColors}
+				onChange={(option) => handleChange('fontColor', option)}
+				title="Цвет шрифта"
+				/>
+
+				<Select
+				selected={selectArticleState.backgroundColor}
+				options={backgroundColors}
+				onChange={(option) => handleChange('backgroundColor', option)}
+				title="Цвет фона"
+				/>
+
+				<Select
+				selected={selectArticleState.contentWidth}
+				options={contentWidthArr}
+				onChange={(option) => handleChange('contentWidth', option)}
+				title="Ширина контента"
+				/>
+
+				<div className={styles.bottomContainer}>
+					<Button title="Сбросить" htmlType="reset" type="clear" />
+					<Button title="Применить" htmlType="submit" type="apply" />
+				</div>
+			</form>
 			</aside>
-		</>
+		</div>
 	);
-};
+}
